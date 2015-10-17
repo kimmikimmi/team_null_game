8de3382d62359bd876dbc28e24f0f65c147eba1a
rtorres12 <rafaeltrrs112@gmail.com>
2015-10-17 18:59:43 -0400

    Initial commit

---
+++ desktop/src/com/uwsoft/platformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.uwsoft.platformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.uwsoft.platformer.PlatformerTutorial;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlatformerTutorial(), config);
+	}
+}
